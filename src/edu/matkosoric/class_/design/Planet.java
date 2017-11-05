968cfef58bc205a34143046bb4b0a1b2bafa166f
matkosoric <soric.matko@gmail.com>
2017-11-05 13:02:45 +0100

    Initial commit

---
+++ src/edu/matkosoric/class_/design/Planet.java
@@ -0,0 +1,52 @@
+package edu.matkosoric.class_.design;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 8 SE, 2017.
+ * Created by © Matko Soric.
+ */
+
+public class Planet {
+
+    // equals method can be overridden and take various properties as a criteria of comparison
+
+
+    public int id;
+    public String name;
+    public boolean innerPlanet;
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+//    @Override         // does not compile, not a proper override
+    public boolean equals(Planet planet) {
+        return this.innerPlanet == planet.innerPlanet;
+    }
+
+    public static void main(String[] args) {
+
+        Planet mercury = new Planet();
+        mercury.id = 1;
+        mercury.name = "Mercury";
+        mercury.innerPlanet = true;
+
+        Planet earth = new Planet();
+        earth.id = 3;
+        earth.name = "Earth";
+        earth.innerPlanet = true;
+
+        Planet jupiter = new Planet();
+        jupiter.id = 5;
+        jupiter.name = "Jupiter";
+        jupiter.innerPlanet = false;
+
+        if(mercury.equals(earth)) System.out.println("Mercury and Earth are inner planets!");
+        else System.out.println("Mercury and Earth are not in the same group of planets!");
+
+        if (earth.equals(jupiter)) System.out.println("Earth and Jupiter are in the same group of planets!");
+        else System.out.println("Earth and Jupiter are not in the same group of planets!");
+
+    }
+}
