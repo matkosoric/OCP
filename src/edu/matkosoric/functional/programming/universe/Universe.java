1a76d8042e4136f13c103ab7469eb307f2d6cbc4
matkosoric <soric.matko@gmail.com>
2017-12-03 12:41:04 +0100

    isEmpty on indefinite stream

---
+++ src/edu/matkosoric/functional/programming/universe/Universe.java
@@ -0,0 +1,23 @@
+package edu.matkosoric.functional.programming.universe;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 8 SE, 2017.
+ * Created by © Matko Soric.
+ */
+
+import java.util.stream.Stream;
+
+public class Universe {
+
+    // immediately after the first mismatch, allMatch returns false
+    // despite the stream being indefinite.
+
+    public static void main(String[] args) {
+
+        Stream<String> voyage = Stream.generate(() -> "go further ");
+        boolean theEnd = voyage.allMatch(String::isEmpty);
+        System.out.println(theEnd);
+
+    }
+}
