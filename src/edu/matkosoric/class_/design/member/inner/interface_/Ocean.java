bab745074567ff3351afba59218a6499a07e2c66
matkosoric <soric.matko@gmail.com>
2017-12-25 16:44:44 +0100

    member inner class with inner interface

---
+++ src/edu/matkosoric/class_/design/member/inner/interface_/Ocean.java
@@ -0,0 +1,46 @@
+package edu.matkosoric.class_.design.member.inner.interface_;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 8 SE, 2017.
+ * Created by © Matko Soric.
+ */
+
+public class Ocean {
+
+    // inner class can use inner interface
+
+    public static void main(String[] args) {
+
+        Fish x = new Ocean().new Fish();
+        x.swim();
+
+        Land.Bear y = new Land().new Bear();
+        y.swim();
+    }
+
+    class Fish implements Swimming{
+        @Override
+        public void swim() {
+            System.out.println("this fish is swimming.");
+        }
+    }
+
+    interface Swimming {
+        public void swim();
+    }
+
+}
+
+class Land {
+
+    // inner member class in other outer class can use the same interface
+
+    class Bear implements Ocean.Swimming {
+        @Override
+        public void swim() {
+            System.out.println("bears are able to swim in this bizarre programming example.");
+        }
+    }
+
+}
