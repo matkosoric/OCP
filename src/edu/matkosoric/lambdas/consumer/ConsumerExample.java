a8f3d0ec0c8fff303cc2b1b36907c01009065047
matkosoric <soric.matko@gmail.com>
2018-01-09 15:16:03 +0100

    consumer interface demo 1

---
+++ src/edu/matkosoric/lambdas/consumer/ConsumerExample.java
@@ -0,0 +1,30 @@
+package edu.matkosoric.lambdas.consumer;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 8 SE, 2018.
+ * Created by © Matko Soric.
+ */
+
+import java.util.function.BooleanSupplier;
+import java.util.function.Consumer;
+
+public class ConsumerExample {
+
+    // Consumer interface does not return anything
+
+    public static void main(String[] args) {
+
+        Consumer<String> consumer1 = System.out::println;
+        Consumer<String> consumer2 = a -> System.out.println(a);
+        Consumer<Integer> consumer3 = System.out::println;
+        Consumer<Boolean> consumer4 = b -> System.out.println(b);
+
+        consumer1.accept("First consumer");
+        consumer2.accept("Second consumer");
+        consumer3.accept(555);
+        consumer4.accept(false);
+
+    }
+
+}
