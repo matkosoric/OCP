27f2b1769d3d826bdb1e83cf32bc613007dedbc0
matkosoric <soric.matko@gmail.com>
2018-01-10 08:51:07 +0100

    consumer interface demo 3

---
+++ src/edu/matkosoric/lambdas/consumer/ConsumerExample3.java
@@ -0,0 +1,37 @@
+package edu.matkosoric.lambdas.consumer;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 8 SE, 2018.
+ * Created by © Matko Soric.
+ */
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BooleanSupplier;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+public class ConsumerExample3 {
+
+    // demonstration of Consumer functional interface
+    // notice the absence of accept() method from the stream case
+
+    public static void main(String[] args) {
+
+        Consumer<Boolean> consumer = a -> System.out.println( a);
+
+        // printing from a stream
+        Stream<Boolean> stream = Stream.of(true, false, false, true, false, true, true, true);
+        stream.forEach(consumer);
+
+        System.out.println("******************");
+
+        // printing from a list
+        List<Boolean> blist = Arrays.asList(true, false, false, true, false, true, true, true);
+        for (Boolean element : blist)
+            consumer.accept(element);
+
+    }
+
+}
