33aa88323c222d18f576d26f72ff61dc7bcb24ec
matkosoric <soric.matko@gmail.com>
2018-01-10 09:54:10 +0100

    predicate interface demo 1

---
+++ src/edu/matkosoric/lambdas/predicate/PredicateExample.java
@@ -0,0 +1,27 @@
+package edu.matkosoric.lambdas.predicate;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 8 SE, 2018.
+ * Created by © Matko Soric.
+ */
+
+import java.util.function.Predicate;
+
+public class PredicateExample {
+
+    // demonstration of Predicate functional interface
+
+    public static void main(String[] args) {
+
+        String stevensonNovel = "Strange Case of Dr. Jekyll and Mr. Hyde";
+        String slauerhoffNovel = "The Forbidden Realm";
+
+        Predicate<String> searchForJekyll = a -> a.contains("Jekyll");
+
+        System.out.println(searchForJekyll.test(stevensonNovel));
+        System.out.println(searchForJekyll.test(slauerhoffNovel));
+
+    }
+
+}
