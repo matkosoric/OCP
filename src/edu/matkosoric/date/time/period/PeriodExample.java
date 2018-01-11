efbe3cffe156859570708f645acb330bec17baec
matkosoric <soric.matko@gmail.com>
2018-01-11 12:17:32 +0100

    Period

---
+++ src/edu/matkosoric/date/time/period/PeriodExample.java
@@ -0,0 +1,28 @@
+package edu.matkosoric.date.time.period;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 8 SE, 2018.
+ * Created by © Matko Soric.
+ */
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+
+public class PeriodExample {
+
+    // Period is used for time interval between two dates
+
+    public static void main(String[] args) {
+
+        Period period = Period.ofYears(1);
+        LocalDate halloween = LocalDate.of(1978, Month.OCTOBER, 31);
+
+        while (halloween.getYear() < 2000) {
+            System.out.println(halloween);
+            halloween = halloween.plus(period);
+        }
+
+    }
+}
---
+++ src/edu/matkosoric/date/time/period/PeriodExample2.java
@@ -0,0 +1,34 @@
+package edu.matkosoric.date.time.period;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 8 SE, 2018.
+ * Created by © Matko Soric.
+ */
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+
+public class PeriodExample2 {
+
+    // demonstration of Period class with Back to the Future dates
+
+    public static void main(String[] args) {
+
+        LocalDate startDate = LocalDate.of (1985, Month.OCTOBER, 26);
+        LocalDate endDate = LocalDate.of(1955, Month.NOVEMBER, 5);
+
+        Period distance = Period.between(startDate, endDate);
+
+        System.out.println(distance);
+
+        // elements of a Period that are 0 are omitted
+        System.out.println(Period.ofMonths(8));
+        System.out.println(Period.of(3, 0, 20));
+        System.out.println(Period.of(3, 0, 40));
+        System.out.println(Period.of(0, 13, 0));
+
+    }
+
+}
