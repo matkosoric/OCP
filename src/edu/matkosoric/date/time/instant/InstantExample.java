5c0ac8afd98c0f13238f5212a7fb9290306617aa
matkosoric <soric.matko@gmail.com>
2018-01-11 16:59:23 +0100

    Instant 2x

---
+++ src/edu/matkosoric/date/time/instant/InstantExample.java
@@ -0,0 +1,37 @@
+package edu.matkosoric.date.time.instant;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 8 SE, 2018.
+ * Created by © Matko Soric.
+ */
+
+import java.time.*;
+
+public class InstantExample {
+
+    // Instant class is used to represent a singular moment in time
+
+    public static void main(String[] args) {
+
+        // creating Instance for current moment
+        Instant now = Instant.now();
+        System.out.println(now);
+        System.out.println(now.atZone(ZoneId.systemDefault()));
+
+        // creating Instance from ZonedDateTime
+        LocalDate date = LocalDate.of(2049, Month.MAY, 20);
+        LocalTime time = LocalTime.of (12, 00);
+        ZoneId zone = ZoneId.of("Asia/Tbilisi");
+        ZonedDateTime zonedDateTime = ZonedDateTime.of(date, time, zone);
+        Instant instant = zonedDateTime.toInstant();
+        System.out.println(instant);
+        System.out.println(zonedDateTime);
+
+        // creating Instant from epoch seconds
+        Instant instant2 = Instant.ofEpochSecond(776588877);
+        System.out.println(instant2);
+
+    }
+
+}
---
+++ src/edu/matkosoric/date/time/instant/InstantExample2.java
@@ -0,0 +1,36 @@
+package edu.matkosoric.date.time.instant;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 8 SE, 2018.
+ * Created by © Matko Soric.
+ */
+
+import java.math.BigInteger;
+import java.time.Duration;
+import java.time.Instant;
+
+public class InstantExample2 {
+
+    // Instant can be easily used to measure time needed for certain task
+
+    public static void main(String[] args) {
+
+        // benchmark for comparing for loop and while loop
+        Instant start = Instant.now();
+        for (int i = 0; i < 100000; i++)
+            {System.out.println(i*i);}
+        Instant end = Instant.now();
+
+        Instant start2 = Instant.now();
+        int i = 0;
+        while (i < 100000)
+            {System.out.println(i*i);
+            i++;}
+        Instant end2 = Instant.now();
+
+        System.out.println("FOR LOOP:" + Duration.between(start, end).toMillis());
+        System.out.println("WHILE LOOP: " + Duration.between(start2, end2).toMillis());
+
+    }
+}
