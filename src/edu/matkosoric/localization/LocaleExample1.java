d2a92ae9f742ebb1b558224f4d753018c65dc647
matkosoric <soric.matko@gmail.com>
2018-01-12 09:17:24 +0100

    default Locale

---
+++ src/edu/matkosoric/localization/LocaleExample1.java
@@ -0,0 +1,24 @@
+package edu.matkosoric.localization;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 8 SE, 2018.
+ * Created by © Matko Soric.
+ */
+
+import java.util.Locale;
+
+public class LocaleExample1 {
+
+    // getting and setting system default Locale
+
+    public static void main(String[] args) {
+
+        Locale locale = Locale.getDefault();
+        System.out.println(locale);
+
+        Locale.setDefault(Locale.ITALY);
+        System.out.println(Locale.getDefault());
+    }
+
+}
