e324580bdf223e4d12ba4e82ebab7b01b984a715
matko <soric.matko@gmail.com>
2018-01-17 15:22:26 +0100

    combining absolute and relative path

---
+++ src/edu/matkosoric/io/parent/child/ParentAndChildPath.java
@@ -0,0 +1,24 @@
+package edu.matkosoric.io.parent.child;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 8 SE, 2018.
+ * Created by © Matko Soric.
+ */
+
+import java.io.File;
+
+public class ParentAndChildPath {
+
+    // getting the path by combining absolute and relative path
+
+    public static void main(String[] args) {
+
+        // note: linux path
+        File parent = new File("/home/matko/IdeaProjects/OCP/src/edu/matkosoric");
+        File child = new File(parent, "io/sample/jelena/rozga/RoditCuTiCerISina.txt");
+
+        System.out.println(child.exists());
+
+    }
+}
