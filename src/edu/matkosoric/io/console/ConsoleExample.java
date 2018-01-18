cba6a6f0b85ede7a141924ea8b12e18f5cfc9892
matko <soric.matko@gmail.com>
2018-01-18 13:20:04 +0100

    Console clas demo

---
+++ src/edu/matkosoric/io/console/ConsoleExample.java
@@ -0,0 +1,33 @@
+package edu.matkosoric.io.console;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 8 SE, 2018.
+ * Created by © Matko Soric.
+ */
+
+import java.io.Console;
+
+public class ConsoleExample {
+
+    // demonstration of Console class
+    // it can not be used from IDE, but can be used from terminal with:
+    // javac edu/matkosoric/io/console/ConsoleExample.java
+    // java edu/matkosoric/io/console/ConsoleExample
+
+    public static void main(String[] args) {
+
+        Console console = System.console();
+
+        if (console != null) {
+
+            String horrorOfUserInput = console.readLine();
+            console.writer().println("Your valuable input is: " + horrorOfUserInput);
+
+        } else {
+            System.out.println("Console is null!");
+        }
+
+    }
+
+}
