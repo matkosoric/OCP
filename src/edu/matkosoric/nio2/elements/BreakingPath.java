e071c4544c4bfeec338bf0e9182480e936a499e6
matko <soric.matko@gmail.com>
2018-01-18 15:01:18 +0100

    elements of Path object

---
+++ src/edu/matkosoric/nio2/elements/BreakingPath.java
@@ -0,0 +1,30 @@
+package edu.matkosoric.nio2.elements;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 8 SE, 2018.
+ * Created by © Matko Soric.
+ */
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class BreakingPath {
+
+    // getting particular elements from the path
+
+    public static void main(String[] args) {
+
+        Path belanPath = Paths.get("/home/matko/IdeaProjects/OCP/src/edu/matkosoric/nio2/sample/neno/belan/divojka_sa_juga.txt");
+
+        System.out.println("Path is: " + belanPath);
+
+        for (int i = 0; i < belanPath.getNameCount(); i++) {
+            System.out.println("Element " + i + " is :" + belanPath.getName(i));
+        }
+
+
+    }
+
+
+}
