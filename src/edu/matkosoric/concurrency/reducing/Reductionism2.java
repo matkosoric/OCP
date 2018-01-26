67ee58a791c727489e162255f3bf40e1fb1e4903
matko <soric.matko@gmail.com>
2018-01-26 10:43:56 +0100

    wrong reduction

---
+++ src/edu/matkosoric/concurrency/reducing/Reductionism2.java
@@ -0,0 +1,31 @@
+package edu.matkosoric.concurrency.reducing;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 8 SE, 2018.
+ * Created by © Matko Soric.
+ */
+
+import java.util.Arrays;
+
+public class Reductionism2 {
+
+    // omitting the combiner and non-associative accumulator will produce different results
+    // for usual stream and parallel stream
+
+    public static void main(String[] args) {
+
+        // ordinal stream
+        System.out.println(Arrays.asList(1,2,3,4,5,6,7,8,9,10)
+            .stream()
+            .reduce(0,(a,b) ->(a-b))
+        );
+
+        // parallel stream
+        System.out.println(Arrays.asList(1,2,3,4,5,6,7,8,9,10)
+                .parallelStream()
+                .reduce(0,(a,b) ->(a-b))
+        );
+
+    }
+}
