b09d9efde001de7c86a30c71c47c492b45801f04
matko <soric.matko@gmail.com>
2018-01-26 14:47:23 +0100

    RecursiveAction on a pizza

---
+++ src/edu/matkosoric/concurrency/recursive/action/PizzaCutting.java
@@ -0,0 +1,49 @@
+package edu.matkosoric.concurrency.recursive.action;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 8 SE, 2018.
+ * Created by © Matko Soric.
+ */
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveAction;
+
+public class PizzaCutting extends RecursiveAction{
+
+    // pizza cutting is sort of a recursive action.
+    // each entity is cut in half until a certain base case is achieved.
+
+
+    private double currentPercentage;
+
+    public PizzaCutting(double currentPercentage) {
+        this.currentPercentage = currentPercentage;
+    }
+
+    @Override
+    protected void compute() {
+
+        // base case, used for ending recursive process
+        if (currentPercentage < 13) {
+            System.out.println("This part is " + currentPercentage + " % of original pizza.");
+        }
+
+        // recursive case that splits certain pizza part in half, that is, two threads
+        else {
+            System.out.println("...splitting...");
+            invokeAll(new PizzaCutting(currentPercentage / 2), new PizzaCutting(currentPercentage / 2));
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+
+        ForkJoinTask<?> task = new PizzaCutting(100);
+        ForkJoinPool pool = new ForkJoinPool();
+        pool.invoke(task);
+
+    }
+}
