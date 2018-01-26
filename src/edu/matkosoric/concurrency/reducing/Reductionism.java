8d859fb62dc3a0ede56bfe09d129481f884f7d3a
matko <soric.matko@gmail.com>
2018-01-26 10:33:31 +0100

    reduction with proper accumulators and combiners

---
+++ src/edu/matkosoric/concurrency/reducing/Reductionism.java
@@ -0,0 +1,32 @@
+package edu.matkosoric.concurrency.reducing;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 8 SE, 2018.
+ * Created by © Matko Soric.
+ */
+
+import java.util.Arrays;
+
+public class Reductionism {
+
+    // reduction with proper accumulators and combiners
+
+    public static void main(String[] args) {
+
+        // both examples produce the same result,
+        // since accumulator and combiner follow suggested requirements
+
+        // ordinary stream
+        System.out.println(Arrays.asList('s', 'o', 'u', 'n', 'd', ' ', 'o', 'f', ' ', 's', 'i', 'l', 'e', 'n', 'c', 'e')
+            .stream()
+            .reduce("", ((a,b) -> a+b), (c,d) -> c+d)
+        );
+
+        // parallel stream
+        System.out.println(Arrays.asList('s', 'o', 'u', 'n', 'd', ' ', 'o', 'f', ' ', 's', 'i', 'l', 'e', 'n', 'c', 'e')
+                .parallelStream()
+                .reduce("", ((a,b) -> a+b), (c,d) -> c+d)
+        );
+    }
+}
