4702bdf0538a672ec00c1942d64cd55222967795
matkosoric <soric.matko@gmail.com>
2018-01-28 19:34:12 +0100

    compareAndSet() method

---
+++ src/edu/matkosoric/concurrency/atomic/classes/ChangeOnce.java
@@ -0,0 +1,38 @@
+package edu.matkosoric.concurrency.atomic.classes;
+
+/*
+ * Code examples for Oracle Certified Associate (OCA) Exam
+ * Java 8 SE, 2017.
+ * Created by © Matko Soric.
+ */
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ChangeOnce implements Runnable{
+
+    // ensure that multiple threads will change static variable only once
+    // solution is in the compareAndSet() method from AtomicInteger
+
+    public AtomicInteger code;
+
+    public void checkCode () {
+        int oldCode = code.get();
+        int newCode = code.get() + 2;
+        code.compareAndSet(oldCode, newCode);
+    }
+
+    public static void main(String[] args) {
+
+        ChangeOnce thread1 = new ChangeOnce();
+        thread1.run();
+        System.out.println(thread1.code.get());
+        thread1.run();
+        System.out.println(thread1.code.get());
+    }
+
+    @Override
+    public void run() {
+        code = new AtomicInteger(4);
+        this.checkCode();
+    }
+}
