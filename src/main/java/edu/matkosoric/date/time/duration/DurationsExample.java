3a15bb64f8a6fb3f1154adb6a23fbee2a81ed5a8
matkosoric <soric.matko@gmail.com>
2021-01-15 09:12:32 +0100

    year update in headers

--- src/main/java/edu/matkosoric/date/time/duration/DurationsExample.java
+++ src/main/java/edu/matkosoric/date/time/duration/DurationsExample.java
@@ -2,7 +2,7 @@ package edu.matkosoric.date.time.duration;
 
 /*
  * Code examples for Oracle Certified Professional (OCP) Exam
- * Java 8 SE, 2018.
+ * Java 8 SE, 2021.
  * Created by © Matko Soric.
  */
 
