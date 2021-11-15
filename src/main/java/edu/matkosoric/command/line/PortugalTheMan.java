af126b03dac4e9b880af28e1a47f1a896615015b
matkosoric <soric.matko@gmail.com>
2021-11-15 17:28:10 +0100

    command line - PortugalTheMan

---
+++ src/main/java/edu/matkosoric/command/line/PortugalTheMan.java
@@ -0,0 +1,18 @@
+package edu.matkosoric.command.line;
+
+public class PortugalTheMan {
+
+    public static void main(String[] args) {
+
+        System.out.println(args [0] + args [1] + args[2]);
+
+        // if run with arguments: "Portugal. The Man" Evil Friends
+        // result will be: Portugal. The ManEvilFriends
+
+        // if run with arguments: Hello "Hello World" Hello World
+        // result will be: HelloHello WorldHello
+
+
+    }
+
+}
