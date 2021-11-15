e2ec80bd56614542e7b04aad7413a47d0d6961d3
matkosoric <soric.matko@gmail.com>
2021-11-15 17:16:41 +0100

    valid and invalid methods - DrugAdministration

---
+++ src/main/java/edu/matkosoric/class_/design/drug/administration/DrugAdministration.java
@@ -0,0 +1,33 @@
+package edu.matkosoric.class_.design.drug.administration;
+
+public class DrugAdministration {
+
+    // which method will compile?
+
+    public boolean inhale(int a) {
+        return a > 0;
+    };
+
+//    Does not compile
+//    Missing return statement. println() does not return anything.
+//    public String inject () {
+//        System.out.println("Injecting");
+//    };
+
+//    Does not compile
+//    type mismatch
+//    public char smoke (String substance) {
+//        return substance;
+//    }
+
+//    Does not compile
+//    void and return are in contradiction
+//    public void snort (int amount) {
+//        return ++amount;
+//    }
+
+    public void swallow () {
+        System.out.println("now");
+    }
+
+}
