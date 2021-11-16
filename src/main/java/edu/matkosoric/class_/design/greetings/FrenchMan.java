e84d0f791724a17ac38ad0e25dc215ac75de74bc
matkosoric <soric.matko@gmail.com>
2021-11-16 17:02:35 +0100

    Bonjour

---
+++ src/main/java/edu/matkosoric/class_/design/greetings/FrenchMan.java
@@ -0,0 +1,13 @@
+package edu.matkosoric.class_.design.greetings;
+
+public class FrenchMan {
+
+    private void print() {
+        System.out.println("Bonjour le monde!");
+    }
+
+    public void bonojur () {
+        print();
+    }
+
+}
---
+++ src/main/java/edu/matkosoric/class_/design/greetings/VisitingUK.java
@@ -0,0 +1,26 @@
+package edu.matkosoric.class_.design.greetings;
+
+public class VisitingUK extends FrenchMan {
+
+//    output is:
+//    Bonjour le monde!
+//    Hello world!
+
+    private void print() {
+        System.out.println("Hello world!");
+    }
+
+    public void hello () {
+        print();
+    }
+
+    public static void main(String[] args) {
+
+        VisitingUK visitingUK = new VisitingUK();
+
+        visitingUK.bonojur();
+        visitingUK.hello();
+
+    }
+
+}
