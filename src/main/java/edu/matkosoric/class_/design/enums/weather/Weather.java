ad602b623556e34a62db606e1e0be3285f4c9b17
matkosoric <soric.matko@gmail.com>
2021-11-16 14:30:09 +0100

    private constructor - Weather

---
+++ src/main/java/edu/matkosoric/class_/design/enums/weather/Weather.java
@@ -0,0 +1,73 @@
+package edu.matkosoric.class_.design.enums.weather;
+
+import java.io.Serializable;
+
+public class Weather {
+
+    // enum constructor compiles only with private scope
+
+    enum WeatherType implements Serializable {
+
+        SUNNY("Sunnny"),
+        RAINING ("Raining"),
+        SNOWING (" Snowing");
+
+        String description;
+
+        private WeatherType (String description) {
+            this.description = description;
+        }
+
+    }
+
+//    // Does not compile
+//    enum WeatherType2 implements Serializable {
+//
+//        SUNNY("Sunnny"),
+//        RAINING ("Raining"),
+//        SNOWING (" Snowing");
+//
+//        String description;
+//
+//        public WeatherType2 (String description) {
+//            this.description = description;
+//        }
+//
+//    }
+
+
+//    // Does not compile
+//    enum WeatherType3 implements Serializable {
+//
+//        SUNNY("Sunnny"),
+//        RAINING ("Raining"),
+//        SNOWING (" Snowing");
+//
+//        String description;
+//
+//        protected WeatherType3 (String description) {
+//            this.description = description;
+//        }
+//
+//    }
+
+//    // Does not compile
+//    enum WeatherType3 implements Serializable {
+//
+//        SUNNY("Sunnny"),
+//        RAINING ("Raining"),
+//        SNOWING (" Snowing");
+//
+//        String description;
+//
+//        package WeatherType3 (String description) {
+//            this.description = description;
+//        }
+//
+//    }
+
+
+}
+
+
+
