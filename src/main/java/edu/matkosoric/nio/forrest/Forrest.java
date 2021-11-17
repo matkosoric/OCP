d47bd28d04b974030be4a972f72eda402db5c96c
matkosoric <soric.matko@gmail.com>
2021-11-17 13:56:03 +0100

    path comparison - tree

---
+++ src/main/java/edu/matkosoric/nio/forrest/Forrest.java
@@ -0,0 +1,29 @@
+package edu.matkosoric.nio.forrest;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Forrest {
+
+    public static void main(String[] args) throws IOException {
+
+        // normalized paths are the same
+        // Files.isSameFile causes NoSuchFileException
+
+        Path path1 = Paths.get("/./forest/./").resolve(Paths.get("tree.txt"));
+        Path path2 = new File("/forest/./water/../tree.txt").toPath();
+
+        System.out.println(path1);
+        System.out.println(path1.normalize());
+        System.out.println(path2);
+        System.out.println(path2.normalize());
+
+        System.out.print(" "+path1.equals(path2));
+        System.out.print(" "+path1.normalize().equals(path2.normalize()));
+
+        System.out.print(Files.isSameFile(path1, path2));  // NoSuchFileException
+    }
+}
