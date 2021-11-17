582dcc7fb2dc22cd314e3b95347d3ad1def10f45
matkosoric <soric.matko@gmail.com>
2021-11-17 16:49:50 +0100

    setters - DNASynth

---
+++ src/main/java/edu/matkosoric/execution/output/dna/DNASynth.java
@@ -0,0 +1,69 @@
+package edu.matkosoric.execution.output.dna;
+
+public class DNASynth {
+
+    int tCount;
+    int cCount;
+    int gCount;
+    int aCount;
+
+    // which two methods modify field values? setGCount() and setAllCounts()
+
+
+    void setACount(int cCount) {
+        cCount = cCount;
+    }
+
+    void setTCount () {
+        this.tCount = tCount;
+    }
+
+    int setCCount () {
+        return cCount;
+    }
+
+    int setGCount (int g) {
+        gCount = g;
+        return gCount;
+    }
+
+    void setAllCounts (int x) {
+        aCount = tCount = this.cCount = setGCount(x);
+    }
+
+
+    @Override
+    public String toString() {
+        return "DNSSynth{" +
+                "tCount=" + tCount +
+                ", cCount=" + cCount +
+                ", gCount=" + gCount +
+                ", aCount=" + aCount +
+                '}';
+    }
+
+    public static void main(String[] args) {
+
+        // baseline case
+        DNASynth dnaSynth = new DNASynth();
+        System.out.println(dnaSynth);
+
+        dnaSynth.setACount(3);
+        System.out.println(dnaSynth);
+
+        dnaSynth.setTCount();
+        System.out.println(dnaSynth);
+
+        dnaSynth.setCCount();
+        System.out.println(dnaSynth);
+
+
+        dnaSynth.setGCount(200);
+        System.out.println(dnaSynth);
+
+        dnaSynth.setAllCounts(300);
+        System.out.println(dnaSynth);
+
+    }
+
+}
---
+++ src/main/java/edu/matkosoric/execution/output/dna/DNASynth2.java
@@ -0,0 +1,52 @@
+package edu.matkosoric.execution.output.dna;
+
+public class DNASynth2 {
+
+    // which settters will have effect
+
+    int tCount;
+    int cCount;
+    int gCount;
+    int aCount;
+
+    int setCCount(int c) {
+        return c;
+    }
+
+    void setGCount(int gCount) {
+        this.gCount = gCount;
+    }
+
+    void setAllCounts(int a,int tCount,int c,int g) {
+//        setCCount(c) = cCount;
+        tCount = tCount;
+        setGCount(g);
+        cCount = setCCount(c);
+        aCount = a;
+    }
+
+    @Override
+    public String toString() {
+        return "DNSSynth{" +
+                "tCount=" + tCount +
+                ", cCount=" + cCount +
+                ", gCount=" + gCount +
+                ", aCount=" + aCount +
+                '}';
+    }
+
+    public static void main(String[] args) {
+
+        // baseline case
+        DNASynth2 dnaSynth = new DNASynth2();
+        System.out.println(dnaSynth);
+
+        dnaSynth.setCCount(55);
+        System.out.println(dnaSynth);
+
+        dnaSynth.setAllCounts(400, 401, 402, 403);
+        System.out.println(dnaSynth);
+
+    }
+
+}
