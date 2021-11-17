1879f8935fbffdd7f8abd326d44a786342dd3882
matkosoric <soric.matko@gmail.com>
2021-11-17 17:59:12 +0100

    formatting date - friday 1997

---
+++ src/main/java/edu/matkosoric/execution/output/friday1997/Friday1997.java
@@ -0,0 +1,34 @@
+package edu.matkosoric.execution.output.friday1997;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Friday1997 {
+
+    public static void main(String[] args) {
+
+        // Which pattern formats the date as: Friday 7th of February 1997?
+        // pattern4 - "eeee d'th of' MMMM yyyy"
+
+        String pattern1 = "eeee dd+'th of'+ MMM yyyy";
+        String pattern2 = "eeee dd'th of' MMM yyyy";
+        String pattern3 = "eeee d+'th of'+ MMMM yyyy";
+        String pattern4 = "eeee d'th of' MMMM yyyy";
+
+        LocalDate localDate = LocalDate.of(1997,2,7);
+
+        DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern(pattern1);
+        System.out.println(dtf1.format (localDate));
+
+        DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern(pattern2);
+        System.out.println(dtf2.format (localDate));
+
+        DateTimeFormatter dtf3 = DateTimeFormatter.ofPattern(pattern3);
+        System.out.println(dtf3.format (localDate));
+
+        DateTimeFormatter dtf4 = DateTimeFormatter.ofPattern(pattern4);
+        System.out.println(dtf4.format (localDate));
+
+    }
+
+}
