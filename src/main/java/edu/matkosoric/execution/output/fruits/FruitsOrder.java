c4cff1d7add5d5152508cd29c8beea16b8ca9ca9
matkosoric <soric.matko@gmail.com>
2021-11-18 12:23:25 +0100

    compareTo with minus - fruits order

---
+++ src/main/java/edu/matkosoric/execution/output/fruits/FruitsOrder.java
@@ -0,0 +1,28 @@
+package edu.matkosoric.execution.output.fruits;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class FruitsOrder {
+
+    // what is the output ?
+    // because of the minus sign ( - a.compareTo(b) ), order is reversed
+
+    public static void main(String[] args) {
+
+        String [] fruitNames = { "appple", "orange", "grape", "lemon" , "apricot", "watermelon"};
+
+        var fruits = new ArrayList<>(Arrays.asList(fruitNames));
+
+        // with minus
+        fruits.sort((var a, var b) -> - a.compareTo(b));
+        fruits.forEach(System.out::println);
+
+        // without minus
+        fruits.sort((var a, var b) -> a.compareTo(b));
+        fruits.forEach(System.out::println);
+
+
+    }
+
+}
