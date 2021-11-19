374db14137296e71b595d741a064302cb896f8cd
matkosoric <soric.matko@gmail.com>
2021-11-19 09:26:55 +0100

    employee filtering & counting

---
+++ src/main/java/edu/matkosoric/streams/employee_filtering/Employee.java
@@ -0,0 +1,48 @@
+package edu.matkosoric.streams.employee_filtering;
+
+import java.time.LocalDate;
+
+public class Employee {
+
+    private String name;
+    private LocalDate birthday;
+    private int salary;
+
+    public Employee(String name, LocalDate birthday, int salary) {
+        this.name = name;
+        this.birthday = birthday;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [name=" + name + ", birthday=" + birthday + ", salary=" + salary + "]";
+    }
+
+
+
+}
---
+++ src/main/java/edu/matkosoric/streams/employee_filtering/EmployeeCount.java
@@ -0,0 +1,33 @@
+package edu.matkosoric.streams.employee_filtering;
+
+import java.time.LocalDate;
+import java.time.chrono.IsoChronology;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class EmployeeCount {
+
+    public static void main(String[] args) {
+
+        List<Employee> roster = new ArrayList<>();
+        roster.add(new Employee("Tim", LocalDate.of(1955, 2, 22), 25));
+        roster.add(new Employee("Rex", LocalDate.of(1963, 1, 4), 10));
+        roster.add(new Employee("Ogi", LocalDate.of(1999, 7, 26), 15));
+        roster.add(new Employee("Buz", LocalDate.of(1989, 10, 12), 20));
+        roster.add(new Employee("Val", LocalDate.of(1997, 9,3), 5));
+
+        Predicate<Employee> p = e -> e.getSalary() > 13;
+        LocalDate cutoffDate = IsoChronology.INSTANCE.date(1980, 1, 1);
+
+        long countEmployees = roster.stream()
+                .filter(p)
+                .filter(e -> e.getBirthday().isAfter(cutoffDate))
+                .peek(element -> System.out.println(element))           // see stream content
+                .count();
+
+        System.out.println(countEmployees);
+
+    }
+
+}
