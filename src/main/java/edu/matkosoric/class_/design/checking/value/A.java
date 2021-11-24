88bd07b625382f9f6dfe74fbdb7318c281ae5661
matkosoric <soric.matko@gmail.com>
2021-11-24 15:33:37 +0100

    private access - compilation fails

---
+++ src/main/java/edu/matkosoric/class_/design/checking/value/A.java
@@ -0,0 +1,9 @@
+package edu.matkosoric.class_.design.checking.value;
+
+public class A {
+
+    private boolean checkValue (int val) {
+        return true;
+    }
+
+}
---
+++ src/main/java/edu/matkosoric/class_/design/checking/value/B.java
@@ -0,0 +1,24 @@
+package edu.matkosoric.class_.design.checking.value;
+
+public class B extends A {
+
+    // what is the result?
+    // compilation fails on line 9 due to private access
+
+//    public int modifyVal (int val) {
+//        if (checkValue(val)) {
+//            return val;
+//        } else {
+//            return 0;
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        B b = new B();
+//        System.out.println(b.modifyVal(10));
+//
+//    }
+
+
+}
