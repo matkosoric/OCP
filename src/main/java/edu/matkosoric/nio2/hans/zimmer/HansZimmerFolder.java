8fcc91e79f1342623160ae75ff155c7899e311b1
matkosoric <soric.matko@gmail.com>
2021-11-26 14:40:59 +0100

    check folder existence hans zimmer

---
+++ src/main/java/edu/matkosoric/nio2/hans/zimmer/HansZimmerFolder.java
@@ -0,0 +1,38 @@
+package edu.matkosoric.nio2.hans.zimmer;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 11 SE, 2021.
+ * Created by © Matko Soric.
+ */
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+
+// #IMPORTANT
+
+public class HansZimmerFolder {
+
+    // what has to be added to line 1 to check whether directory exist ?
+    // if folder exists, prints true
+    // otherwise, throws exception
+
+    public static void main(String[] args) {
+        try {
+            Path path = Paths.get("c:/music/hans_zimmer");
+
+//            BasicFileAttributes attributes1 = Files.isDirectory (path);                                 // does not compile - type mismatch
+//            BasicFileAttributes attributes2 =Files.getAttribute (path, "insdirectory");                 // does not compile - type mismatch
+//            BasicFileAttributes attributes3 = Files, readAttributes (path, FileAttributes, class);      // does not compile - wrong syntax
+            BasicFileAttributes attributes4 = Files.readAttributes(path, BasicFileAttributes.class); // line 1
+
+            System.out.println(attributes4.isDirectory());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
