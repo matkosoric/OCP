ff90d20e2e6844ed87ab190fb4dcc5d60eb2606a
matkosoric <soric.matko@gmail.com>
2021-11-26 14:59:37 +0100

    static initialization - kepler

---
+++ src/main/java/edu/matkosoric/execution/output/johannes/kepler/Johannes.java
@@ -0,0 +1,27 @@
+package edu.matkosoric.execution.output.johannes.kepler;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 11 SE, 2021.
+ * Created by © Matko Soric.
+ */
+
+public class Johannes {
+
+    // what is the result of the code?
+    // compilation fails, static y variable not initialised
+
+    private final int x = 1;
+
+    //    static final int y;         // breaks compilation
+    public Johannes() {
+        System.out.println(x);
+//        System.out.println(y);
+    }
+
+    public static void main(String[] args) {
+        new Johannes();
+    }
+
+
+}
