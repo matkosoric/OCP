f51299443b7a3521455e61e750162715bf3a7204
matkosoric <soric.matko@gmail.com>
2021-12-03 16:49:19 +0100

    Bounded types with generics

---
+++ src/main/java/edu/matkosoric/execution/output/extending/numbers/ExtendingNumbers.java
@@ -0,0 +1,27 @@
+package edu.matkosoric.execution.output.extending.numbers;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 11 SE, 2021.
+ * Created by © Matko Soric.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExtendingNumbers {
+
+    // what lists compile ?
+
+
+    List<? super Short> list1 = new ArrayList<Number>();
+
+//    List<? super Number> list2 = new ArrayList<Integer>();      // does not compile
+//
+    List<? extends Number> list3 = new ArrayList<Byte>();
+
+//    List<? extends Number> list4 = new ArrayList<Object>();      // does not compile
+
+//    List<? super Float> list5 = new ArrayList<Double>();       // does not compile
+
+}
