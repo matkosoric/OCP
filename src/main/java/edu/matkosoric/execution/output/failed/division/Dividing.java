25a9967769bdc8361ae0e754d48602c1697f4db9
matkosoric <soric.matko@gmail.com>
2021-12-03 15:49:07 +0100

    duplicated iterator name - french republic

--- src/main/java/edu/matkosoric/execution/output/failed/division/Dividing.java
+++ src/main/java/edu/matkosoric/execution/output/failed/division/Dividing.java
@@ -6,10 +6,10 @@ public class Dividing {
 
         // does not compile, variable z is not declared
 
-//        float x=2, y=4;
-//        float a =y/x, b=y/z;
-//        if(a>b){
-//            System.out.println(a+b);
+//        float x = 2, y = 4;
+//        float a = y / x, b = y / z;
+//        if (a > b) {
+//            System.out.println(a + b);
 //        }
 
     }
---
+++ src/main/java/edu/matkosoric/execution/output/french/republic/FrenchRepublic.java
@@ -0,0 +1,48 @@
+package edu.matkosoric.execution.output.french.republic;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 11 SE, 2021.
+ * Created by © Matko Soric.
+ */
+
+import java.util.Iterator;
+import java.util.List;
+
+public class FrenchRepublic {
+
+    // what breaks compilation?
+    // iter2 in second for loop
+
+    public static void main(String[] args) {
+
+       Iterator iter1 = List.of (1870, 1946, 1958).iterator();
+       while (iter1.hasNext()) {
+           dummyMethod(iter1.next());
+       }
+
+        Iterator iter2 = List.of (1870, 1946, 1958).iterator();
+        while (iter1.hasNext()) {                                   // notice iter1 !
+            dummyMethod2(iter2.next());
+        }
+
+        for (Iterator iter = List.of(1870, 1946, 1958).iterator(); iter.hasNext();) {
+            dummyMethod(iter.hasNext());
+        }
+
+//        for (Iterator iter2 = List.of(1870, 1946, 1958).iterator(); iter.hasNext();) {
+//            dummyMethod2(iter2.hasNext());
+//        }
+
+
+    }
+
+    public static void dummyMethod (Object iteratedObject) {
+        System.out.println("dummy");
+    }
+
+    public static void dummyMethod2 (Object iteratedObject) {
+        System.out.println("dummy 2");
+    }
+
+}
