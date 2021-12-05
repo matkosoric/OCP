df7f661c4e0c286f5693a478eef31edb43856903
matkosoric <soric.matko@gmail.com>
2021-12-05 16:07:08 +0100

    functional interface Function - salary

---
+++ src/main/java/edu/matkosoric/lambdas/salary/doubling/SalaryDoubling.java
@@ -0,0 +1,37 @@
+package edu.matkosoric.lambdas.salary.doubling;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 11 SE, 2021.
+ * Created by © Matko Soric.
+ */
+
+import java.util.List;
+import java.util.function.Function;
+
+public class SalaryDoubling {
+
+    public static void main(String[] args) {
+
+        var salaries = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        StringBuilder sb = new StringBuilder();
+
+        Function<Integer, Integer> f = n -> n * 2;
+//    Function<Integer> f = n -> n * 2;             // does not compile
+//    Function<int> f = n -> n * 2;                 // does not compile
+//    Function<int, int> f = n -> n * 2;            // does not compile
+//    Function f = n -> n * 2;                      // does not compile
+
+        for (int a : salaries) {
+            sb.append(f.apply(a));
+            sb.append(" ");
+        }
+
+        System.out.println(sb);
+
+    }
+
+
+
+}
--- src/main/java/edu/matkosoric/streams/widget/WidgetExecution.java
+++ src/main/java/edu/matkosoric/streams/widget/WidgetExecution.java
@@ -15,7 +15,7 @@ public class WidgetExecution {
     public static void main(String[] args) {
 
         List widgets = List.of(new Widget("Basic Widget", 19.55),
-                new Widget ("Enhanced Widget", 35.00),
+                new Widget("Enhanced Widget", 35.00),
                 new Widget("Luxury Edition Widget", 55.45)
         );
 
@@ -24,8 +24,8 @@ public class WidgetExecution {
 //        widgetStream.filter(a -> a.getPrice() > 20.00)                // does not compile
 //                .forEach(System.out::println);
 
-        widgetStream.filter(a -> ((Widget)a).getPrice() > 20.00)        // compiles with Widget casting
-        .forEach(System.out::println);
+        widgetStream.filter(a -> ((Widget) a).getPrice() > 20.00)        // compiles with Widget casting
+                .forEach(System.out::println);
 
 //        List<Widget> widgetStream2 = widgets.stream();                // does not compile
 
