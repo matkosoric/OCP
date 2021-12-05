9cb521f58d40922bbd2163fd08a040f0d6b1b62e
matkosoric <soric.matko@gmail.com>
2021-12-05 19:45:36 +0100

    for loop - boxing

---
+++ src/main/java/edu/matkosoric/execution/output/boxing/CountingPunches.java
@@ -0,0 +1,23 @@
+package edu.matkosoric.execution.output.boxing;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 11 SE, 2021.
+ * Created by © Matko Soric.
+ */
+
+public class CountingPunches {
+
+    // what is the output?
+    // 1 3 5 7 9
+
+    public static void main(String[] args) {
+
+        int i = 0;
+        for (; i < 10; i++) {
+            System.out.print(++i + " ");
+        }
+
+    }
+
+}
