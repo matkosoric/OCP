452652b058b72318969e8a8600ae6cc24295e834
matkosoric <soric.matko@gmail.com>
2021-12-05 19:39:09 +0100

    extending with collection - athlete

---
+++ src/main/java/edu/matkosoric/class_/design/athlete/Athlete.java
@@ -0,0 +1,19 @@
+package edu.matkosoric.class_.design.athlete;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 11 SE, 2021.
+ * Created by © Matko Soric.
+ */
+
+import java.util.Collection;
+
+public class Athlete {
+
+    private Collection collection;
+
+    public void set(Collection collection) {
+        this.collection = collection;
+    }
+
+}
---
+++ src/main/java/edu/matkosoric/class_/design/athlete/ProfessionalAthlete4.java
@@ -0,0 +1,19 @@
+package edu.matkosoric.class_.design.athlete;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 11 SE, 2021.
+ * Created by © Matko Soric.
+ */
+
+import java.util.Map;
+
+public class ProfessionalAthlete4 extends Athlete {
+
+    // what can be used as method body to compile this class?
+
+    public void set(Map<String, String> map) {
+//        super.set(List<String> map);      // does not compile
+    }
+
+}
