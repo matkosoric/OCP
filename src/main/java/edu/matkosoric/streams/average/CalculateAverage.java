1611098a0f65228fe8bd1abffb8ef58ded2b437e
matkosoric <soric.matko@gmail.com>
2021-12-09 17:17:47 +0100

    calculate average in stream

---
+++ src/main/java/edu/matkosoric/streams/average/CalculateAverage.java
@@ -0,0 +1,30 @@
+package edu.matkosoric.streams.average;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 11 SE, 2021.
+ * Created by © Matko Soric.
+ */
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CalculateAverage {
+
+    // pick streams that calculate average value
+
+    public static void main(String[] args) {
+
+        var numbers = List.of(0,1,2,3,4,5,6,7,8,9);
+
+//        double avg1 = numbers.stream().parallel().averagingDouble(a -> a);                  // does not compile
+        double avg2 = numbers.parallelStream().mapToInt (m -> m).average().getAsDouble ();
+//        double avg3 = numbers.stream().mapToInt (i -> i).average().parallel();              // does not compile
+//        double avg4 = numbers.stream().average().getAsDouble();                             // does not compile
+        double avg5 = numbers.stream().collect(Collectors.averagingDouble(n -> n));
+
+        System.out.println(avg2);
+        System.out.println(avg5);
+    }
+
+}
