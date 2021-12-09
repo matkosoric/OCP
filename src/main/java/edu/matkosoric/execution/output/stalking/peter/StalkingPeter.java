f0429d43fc1637c1ff7d0406c57bf21142ddde80
matkosoric <soric.matko@gmail.com>
2021-12-09 17:13:21 +0100

    IndexOutOfBoundsException example

---
+++ src/main/java/edu/matkosoric/execution/output/stalking/peter/StalkingPeter.java
@@ -0,0 +1,29 @@
+package edu.matkosoric.execution.output.stalking.peter;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 11 SE, 2021.
+ * Created by © Matko Soric.
+ */
+
+import java.util.ArrayList;
+
+public class StalkingPeter {
+
+    // what is the output?
+    // runtime error: java.lang.IndexOutOfBoundsException
+
+    public static void main(String[] args) {
+
+        var data = new ArrayList<>();
+        data.add("Peter");
+        data.add(30);
+        data.add("Market Road");
+        data.set(1, 25);
+        data.remove(2);
+        data.set(3, 1000L);
+        System.out.print(data);
+
+    }
+
+}
