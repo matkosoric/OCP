72eb73c4a6c6db92f5b10779313fc6a348618bfe
matkosoric <soric.matko@gmail.com>
2021-12-10 14:21:29 +0100

    weaker access privileges - human animal

---
+++ src/main/java/edu/matkosoric/class_/design/animal/human/a/Animal.java
@@ -0,0 +1,21 @@
+package edu.matkosoric.class_.design.animal.human.a;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 11 SE, 2021.
+ * Created by © Matko Soric.
+ */
+
+public abstract class Animal {
+
+    protected abstract void walk_1();
+
+    protected abstract void walk_2();
+
+    protected abstract void walk_3();
+
+    protected abstract void walk_4();
+
+    protected abstract void walk_5();
+
+}
---
+++ src/main/java/edu/matkosoric/class_/design/animal/human/h/Human.java
@@ -0,0 +1,21 @@
+package edu.matkosoric.class_.design.animal.human.h;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 11 SE, 2021.
+ * Created by © Matko Soric.
+ */
+
+import edu.matkosoric.class_.design.animal.human.a.Animal;
+
+public abstract class Human extends Animal {
+
+    // which of the following methods are compiling?
+
+    protected void walk_1(){};
+//    void walk_2(){};                   // does not compile - weaker access privileges
+//    abstract void walk_3();            // does not compile - weaker access privileges
+//    private void walk_4(){};           // does not compile - weaker access privileges
+    public abstract void walk_5();
+
+}
