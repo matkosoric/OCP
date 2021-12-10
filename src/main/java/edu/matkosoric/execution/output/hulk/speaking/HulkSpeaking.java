12c0c42649265fc024437b92cdcb0dfd35383871
matkosoric <soric.matko@gmail.com>
2021-12-10 14:06:38 +0100

    nested array walk

---
+++ src/main/java/edu/matkosoric/execution/output/hulk/speaking/HulkSpeaking.java
@@ -0,0 +1,27 @@
+package edu.matkosoric.execution.output.hulk.speaking;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 11 SE, 2021.
+ * Created by © Matko Soric.
+ */
+
+public class HulkSpeaking {
+
+    // what is the output?
+    // gj hk il
+
+    public static void main(String[] args) {
+
+        char[][] arrays = {{'g', 'j'}, {'h', 'k'}, {'i', 'l'}};
+
+        for (char[] xx : arrays) {
+            for (char yy : xx) {
+                System.out.print(yy);
+            }
+            System.out.print(" ");
+        }
+
+    }
+
+}
