c544cbf9ce9a1dda7f4413dae9a1d3a68998030e
matkosoric <soric.matko@gmail.com>
2021-12-13 15:12:06 +0100

    prrt - continue a

---
+++ src/main/java/edu/matkosoric/execution/output/prrt/PRRT.java
@@ -0,0 +1,38 @@
+package edu.matkosoric.execution.output.prrt;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 11 SE, 2021.
+ * Created by © Matko Soric.
+ */
+
+// #TAG1
+
+public class PRRT {
+
+    // what has to be added to line 1 in order to output PRRT
+    // continue a;
+
+    public static void main(String[] args) {
+        StringBuilder txt1 = new StringBuilder("PPQRRRSTT");
+        int i = 0;
+        a:
+        while (i < txt1.length()) {
+            char x = txt1.charAt(i);
+            int j = 0;
+            i++;
+            b:
+            while (j < txt1.length()) {
+                char y = txt1.charAt(j);
+                if (i != j && y == x) {
+                    txt1.deleteCharAt(j);
+                    continue a;      //LINE 1
+                }
+                j++;
+
+            }
+        }
+        System.out.println(txt1);
+    }
+
+}
