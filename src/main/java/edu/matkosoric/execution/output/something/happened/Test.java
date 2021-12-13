70d1826c1c3c7c952329dd97ec42edf144fd0ac2
matkosoric <soric.matko@gmail.com>
2021-12-13 16:02:23 +0100

    something happened

---
+++ src/main/java/edu/matkosoric/execution/output/something/happened/Test.java
@@ -0,0 +1,43 @@
+package edu.matkosoric.execution.output.something.happened;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 11 SE, 2021.
+ * Created by © Matko Soric.
+ */
+
+public class Test {
+
+    // in order to print "Something happened", what should
+    // GeneralException and SpecificException extend?
+    // Exception and GeneralException
+
+    public static void doThings() throws GeneralException {
+        try {
+            throw new RuntimeException("Something happened");
+        } catch (Exception e) {
+            throw new SpecificException(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            Test.doThings();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+
+class GeneralException extends Exception {                  /* line 1 */
+    public GeneralException(String s) {
+        super(s);
+    }
+}
+
+class SpecificException extends GeneralException {         /* line 2 */
+    public SpecificException(String s) {
+        super(s);
+    }
+}
