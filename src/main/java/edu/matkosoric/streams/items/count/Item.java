823284e76b391288b92155e4719bb56def4d7163
matkosoric <soric.matko@gmail.com>
2021-12-13 14:56:52 +0100

    filter stream

---
+++ src/main/java/edu/matkosoric/streams/items/count/Item.java
@@ -0,0 +1,18 @@
+package edu.matkosoric.streams.items.count;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 11 SE, 2021.
+ * Created by © Matko Soric.
+ */
+
+public class Item {
+
+    public String name;
+    public int count;
+
+    public Item(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+}
---
+++ src/main/java/edu/matkosoric/streams/items/count/ItemCount.java
@@ -0,0 +1,33 @@
+package edu.matkosoric.streams.items.count;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 11 SE, 2021.
+ * Created by © Matko Soric.
+ */
+
+import java.util.List;
+
+public class ItemCount {
+
+    // find item with count below zero
+
+    public static void main(String[] args) {
+
+        var items = List.of(new Item("A", 10),
+                new Item("B", 2),
+                new Item("C", 12),
+                new Item("D", 5),
+                new Item("E", -6));
+
+        if (items.stream().anyMatch(i -> i.count < 0)) {
+            System.out.println("There is an item for which the variable count is below zero.");
+        }
+
+        if (items.stream().filter(i -> i.count < 0).findAny().isPresent()) {
+            System.out.println("There is an item for which the variable count is below zero.");
+        }
+
+    }
+
+}
