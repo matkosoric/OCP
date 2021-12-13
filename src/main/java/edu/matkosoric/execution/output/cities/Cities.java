c0e707eb789c5ecb1266b36c107fa9bc47fe93ce
matkosoric <soric.matko@gmail.com>
2021-12-13 15:38:10 +0100

    output cities

---
+++ src/main/java/edu/matkosoric/execution/output/cities/Cities.java
@@ -0,0 +1,53 @@
+package edu.matkosoric.execution.output.cities;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 11 SE, 2021.
+ * Created by © Matko Soric.
+ */
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.IntConsumer;
+import java.util.stream.Stream;
+
+public class Cities {
+
+    // what is the output?
+
+    // Sydney
+    // Dhaka
+    // New York
+    // London
+    // SYDNEY
+    // DHAKA
+    // NEW YORK
+    // LONDON
+    // 1
+
+    public static void main(String[] args) {
+        Consumer<String> printConsumer = t -> System.out.println(t);
+        Stream<String> cities = Stream.of("Sydney", "Dhaka", "New York", "London");
+
+        cities.forEach(printConsumer);
+
+        List<String> citiesList = Arrays.asList("Sydney", "Dhaka", "New York", "London");
+
+        Consumer<List<String>> upperCaseConsumer = list -> {
+            for (int i = 0; i < list.size(); i++) {
+                list.set(i, list.get(i).toUpperCase());
+            }
+        };
+
+
+        Consumer<List<String>> printConsumerList = list -> list.stream().forEach(System.out::println);
+
+        upperCaseConsumer.andThen(printConsumerList).accept(citiesList);
+
+        IntConsumer intConsumer = t -> System.out.println(t);
+        intConsumer.accept(1);
+
+    }
+
+}
