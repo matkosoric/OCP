35f096a7bb94f678f205304593f72045ccf99b5b
matkosoric <soric.matko@gmail.com>
2021-12-14 09:28:49 +0100

    unmodified parameter - tulip

---
+++ src/main/java/edu/matkosoric/execution/output/garden/Garden.java
@@ -0,0 +1,35 @@
+package edu.matkosoric.execution.output.garden;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 11 SE, 2021.
+ * Created by © Matko Soric.
+ */
+
+class Plant {
+}
+
+class Tulip extends Plant {
+}
+
+public class Garden {
+
+    // what is the output?
+    // Take extra care
+    // Take extra care
+
+    private static Plant plant;
+
+    public static void main(String[] args) {
+        plant = new Tulip();
+        feed(plant);
+        feed(plant);
+    }
+
+    public static void feed(Plant plant) {
+        if (plant instanceof Tulip) {
+            System.out.println("Take extra care");
+        }
+        plant = null;
+    }
+}
