8c827afabe580e05d5dc916db0746ca176b73e52
matkosoric <soric.matko@gmail.com>
2021-12-15 11:56:41 +0100

    files find - continent

---
+++ src/main/java/edu/matkosoric/nio2/continent/Continent.java
@@ -0,0 +1,36 @@
+package edu.matkosoric.nio2.continent;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam
+ * Java 11 SE, 2021.
+ * Created by © Matko Soric.
+ */
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.function.BiPredicate;
+
+public class Continent {
+
+    // what is the output?
+    // ...\src\main\java\edu\matkosoric\nio2\continent
+    // ...\src\main\java\edu\matkosoric\nio2\continent\country
+
+    public static void main(String[] args) {
+
+        BiPredicate<Path, BasicFileAttributes> pred = (path, fileAttrs) -> {
+            return fileAttrs.isDirectory();
+        };
+        int depth = 1;
+        try (var stream = Files.find(Paths.get(System.getProperty("user.dir") + "\\src\\main\\java\\edu\\matkosoric\\nio2\\continent\\"), depth, pred)) {
+            stream.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
