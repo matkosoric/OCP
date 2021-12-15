db58d4c3164d67d4693a12cb35a053d2b374ed92
matkosoric <soric.matko@gmail.com>
2021-12-15 14:04:15 +0100

    x-y-- computer

---
+++ src/main/java/edu/matkosoric/class_/design/computer/Tester.java
@@ -0,0 +1,26 @@
+package edu.matkosoric.class_.design.computer;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam 1Z0-819
+ * Java 11 SE, 2021.
+ * Created by © Matko Soric.
+ */
+
+// does not compile
+
+//public class Tester {
+//    public static int reduce(int x) {
+//        int y = 4;
+//        class Computer {
+//            int reduce(int x) {
+//                return x-y--;       // point of failure for compilation
+//            }
+//        }
+//        Computer a = new Computer();
+//        return a.reduce(x);
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(reduce(1));
+//    }
+//}
