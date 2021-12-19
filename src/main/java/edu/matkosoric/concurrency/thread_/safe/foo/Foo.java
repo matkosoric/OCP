9227c04dfd1f6f62d16bfaaa157719e17fb12402
matkosoric <soric.matko@gmail.com>
2021-12-19 15:13:22 +0100

    ReentrantLock with fairness

---
+++ src/main/java/edu/matkosoric/concurrency/thread_/safe/foo/Foo.java
@@ -0,0 +1,28 @@
+package edu.matkosoric.concurrency.thread_.safe.foo;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam 1Z0-819
+ * Java 11 SE, 2021.
+ * Created by © Matko Soric.
+ */
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Foo {
+
+    // how to make Foo class thread safe?
+    // use fairness in constructor
+    // new ReentrantLock(true)
+
+    private Thread.State state;
+    private final ReentrantLock lock = new ReentrantLock();
+
+    public void foo() throws Exception {
+        try {
+            lock.lock();
+//            state.mutate();     // does not compile
+        } finally {
+            lock.unlock();
+        }
+    }
+}
