907d6bb7f19f90606d83c2eb9abc27a9d75e6935
matkosoric <soric.matko@gmail.com>
2021-12-19 15:24:49 +0100

    implement method z - soylent green

---
+++ src/main/java/edu/matkosoric/class_/design/soylent/green/SoylentGreen.java
@@ -0,0 +1,34 @@
+package edu.matkosoric.class_.design.soylent.green;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam 1Z0-819
+ * Java 11 SE, 2021.
+ * Created by © Matko Soric.
+ */
+
+// #TAG2
+
+public class SoylentGreen {
+
+    // how to change code so it compiles?
+    // at (at position 3)
+    // public void z() { }
+}
+
+
+interface A {
+    abstract void x();
+}
+
+abstract class B /* position 1*/ {
+
+    /* position 2 */
+    public void x() {
+    }
+
+    public abstract void z();
+}
+
+class C extends B implements A {
+    public void z() { } /* position 3 */
+}
---
+++ src/main/java/edu/matkosoric/collections/marvel/HardToForgetExample.java
@@ -0,0 +1,34 @@
+package edu.matkosoric.collections.marvel;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam 1Z0-819
+ * Java 11 SE, 2021.
+ * Created by © Matko Soric.
+ */
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+public class HardToForgetExample {
+
+    // what is the output?
+    // 3 5 3 3
+
+    public static void main(String[] args) {
+
+        List<String> list1 = new LinkedList<String>();
+        Set<String> hs1 = new HashSet<String>();
+        String[] v = {"a", "b", "c", "b", "a"};
+        for (String s : v) {
+            list1.add(s);
+            hs1.add(s);
+        }
+        System.out.print(hs1.size() + " " + list1.size() + " ");
+        HashSet hs2 = new HashSet(list1);
+        LinkedList list2 = new LinkedList(hs1);
+        System.out.print(hs2.size() + " " + list2.size());
+
+    }
+}
