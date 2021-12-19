71a7e3ac29cf49fbad66578df794c279768ca821
matkosoric <soric.matko@gmail.com>
2021-12-19 13:44:41 +0100

    optional duke

---
+++ src/main/java/edu/matkosoric/execution/output/duke/Main.java
@@ -0,0 +1,31 @@
+package edu.matkosoric.execution.output.duke;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam 1Z0-819
+ * Java 11 SE, 2021.
+ * Created by © Matko Soric.
+ */
+
+import java.util.Optional;
+
+// #TAG2
+
+public class Main {
+
+    // what is the output?
+    // Duke
+
+    public static void main(String[] args) {
+
+        Optional<String> value = createValue();
+        String str = value.orElse("Duke");
+        System.out.println(str);
+
+    }
+
+    private static Optional<String> createValue() {
+        String s = null;
+        return Optional.ofNullable(s);
+    }
+
+}
