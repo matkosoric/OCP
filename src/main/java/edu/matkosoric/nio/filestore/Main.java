feeb487b8741964af68b602c0eb1ffda5c196190
matkosoric <soric.matko@gmail.com>
2021-12-19 13:48:47 +0100

    deleting non existing filestore.txt

---
+++ src/main/java/edu/matkosoric/nio/filestore/Main.java
@@ -0,0 +1,34 @@
+package edu.matkosoric.nio.filestore;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam 1Z0-819
+ * Java 11 SE, 2021.
+ * Created by © Matko Soric.
+ */
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+// #TAG2
+
+public class Main {
+
+    // what is the output if file foes not exist?
+    // \u01\work\filestore.txtis not deleted.
+
+    public static void main(String[] args) {
+
+        try {
+            Path path = Paths.get("/u01/work/filestore.txt");
+            boolean result = Files.deleteIfExists(path);
+            if (result) System.out.println(path + "is deleted.");
+            else System.out.println(path + "is not deleted.");
+        } catch (IOException e) {
+            System.out.println("Exception");
+        }
+
+    }
+
+}
