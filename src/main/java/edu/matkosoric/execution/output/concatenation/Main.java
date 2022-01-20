39e60d2f76d9f8a51c7f8dd324de38822c9e9f44
matkosoric <soric.matko@gmail.com>
2022-01-20 15:09:31 +0100

    int concatenation

---
+++ src/main/java/edu/matkosoric/execution/output/concatenation/Main.java
@@ -0,0 +1,21 @@
+package edu.matkosoric.execution.output.concatenation;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam 1Z0-819
+ * Java 11 SE, 2022.
+ * Created by © Matko Soric.
+ */
+
+public class Main {
+
+    // what is the output?
+    // 6=x+y=42
+
+    public static void main(String[] args) {
+
+        int x = 4;
+        int y = 2;
+        System.out.println(x + y + "=x+y=" + x + y);
+
+    }
+}
