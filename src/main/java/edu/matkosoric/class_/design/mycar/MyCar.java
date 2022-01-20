5c7b7aba9bfcd0dcfb3c3d8b384e4778a9da6d2f
matkosoric <soric.matko@gmail.com>
2022-01-20 13:50:59 +0100

    compiling empty class

---
+++ src/main/java/edu/matkosoric/class_/design/mycar/MyCar.java
@@ -0,0 +1,14 @@
+package edu.matkosoric.class_.design.mycar;
+
+/*
+ * Code examples for Oracle Certified Professional (OCP) Exam 1Z0-819
+ * Java 11 SE, 2022.
+ * Created by © Matko Soric.
+ */
+
+public class MyCar {
+
+    // what happens if you run javac C:\Users\matko.soric\IdeaProjects\OCP\src\main\java\edu\matkosoric\class_\design\mycar\MyCar.java ?
+    // compilation without errors or warnings
+
+}
